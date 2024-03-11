4a68ee882d5cd955d714c9df10c87a300978b619
Miguel <mg0847091@gmail.com>
2024-03-11 06:34:03 -0500

    11 commit

--- Tatto/settings.py
+++ Tatto/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'TattoApp/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'TattoApp/static'),)
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
